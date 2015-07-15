a39ad6beb31b705542aa4b40c220c319a25fb841
bobsturm <bobsturm@gmail.com>
2015-07-15 12:56:22 -0500

    no message

---
+++ octave/gradientDescent.m
@@ -0,0 +1,42 @@
+function [theta, J_history] = gradientDescent(X, y, theta, alpha, num_iters)
+%GRADIENTDESCENT Performs gradient descent to learn theta
+
+m = length(y); % number of training examples
+
+if (num_iters < 0)
+	error ('num_iters cannot be negative');
+endif
+
+if (alpha <= 0.0)
+	error ('alpha must be positive');
+endif
+
+if (size(X,1) != m)
+	error ('number of rows in X is != length of y');
+endif
+
+if (size(theta, 1) != size(X, 2))
+	error ('number of rows in theta != num cols in X')
+endif
+
+if (sum(X(:, 1) != ones(m, 1)) != 0)
+	error ('not all values in first column of X are set to 1')
+endif
+
+if (m == 0)
+	return;
+endif
+
+J_history = zeros(num_iters, 1);
+
+for iter = 1:num_iters
+	
+	costs = ((X * theta) - y);
+	presum = bsxfun(@times, costs, X);
+  delta = ((1 / m) * sum(presum, 1));
+  theta = theta - (alpha * (delta'));
+    
+  J_history(iter) = computeCost(X, y, theta);
+end
+
+endfunction
---
+++ octave/testGradientDescent.m
@@ -0,0 +1,57 @@
+function testGradientDescent
+	assert(false, 'This function should only be called in test mode!');
+
+%!function shouldRaiseErrorWhenNumItersNegative
+%! fail ('gradientDescent([], [], [], 0.0, -1)');
+%!endfunction
+
+%!function shouldRaiseErrorWhenAlphaNegative
+%! fail ('gradientDescent([1 3], [7], [1; 2], -0.01, 1)');
+%!endfunction
+
+%!function shouldRaiseErrorWhenNumRowsInXDoesNotMatchY
+%! fail ('gradientDescent([], [1], [], 0.01, 10)');
+%! fail ('gradientDescent([1], [1;2], [], 0.01, 10)');
+%! fail ('gradientDescent([1], [], [], 0.01, 10)');
+%!endfunction
+
+%!function shouldRaiseErrorWhenFirstColInXNotSetToZeros
+%! fail ('gradientDescent([0], [1], [1], 0.01, 10)');
+%! fail ('gradientDescent([2], [3], [], 0.01, 10)');
+%!endfunction
+
+%!function shouldRaiseErrorWhenNumRowsInThetaDoNotMatchNumColsInX
+%! fail ('computeCost([0], [1], [1; 2])');
+%! fail ('computeCost([1 2; 2 3], [3; 7], [0.5])');
+%! fail ('computeCost([1 2], [3], [])');
+%!endfunction
+
+%!function shouldReturnSameThetaWhenNumItersIsZero
+%! [theta, J_history] = gradientDescent([1 0], [1], [1; 2], 0.01, 0);
+%! assert(theta, [1; 2]);
+%! [theta, J_history] = gradientDescent([1 0 2], [1], [0.5; 3.1; 7.76], 0.01, 0);
+%! assert(theta, [0.5; 3.1; 7.76]);
+%!endfunction
+
+%!function shouldReturnCorrectValuesForThetaVector
+%! %theta = gradientDescent([1 0 1; 1 2 3], [2; 4], [0; 1; 2], 2, 1);
+%! %assert(theta, [-4; -7; -10]);
+%! %theta = gradientDescent([1 0], [1], [0; 1], 1, 1);
+%! %assert(theta, [1; 1]);
+%! %theta = gradientDescent([1 1 1 1; 1 2 2 2; 1 3 3 3], [10; 8; 6], [1; 1; 1; 1], 1, 1);
+%! %assert(theta, [2; -(1/3); -(1/3); -(1/3)], 0.00001);
+%! theta = gradientDescent([1 1 1 1; 1 2 2 2; 1 3 3 3], [10; 8; 6], [1; 1; 1; 1], 1, 2);
+%! assert(theta, [10; 15; 15; 15], 0.00001);
+%! theta = gradientDescent([1 1 1 1; 1 2 2 2; 1 3 3 3], [10; 8; 6], [1; 1; 1; 1], 1, 3);
+%! assert(theta, [-82; -200.33; -200.33; -200.33], 0.01);
+%!endfunction
+
+%!test
+%! shouldRaiseErrorWhenNumItersNegative();
+%! shouldRaiseErrorWhenAlphaNegative();
+%! shouldRaiseErrorWhenNumRowsInXDoesNotMatchY();
+%! shouldRaiseErrorWhenFirstColInXNotSetToZeros();
+%! shouldRaiseErrorWhenNumRowsInThetaDoNotMatchNumColsInX();
+%! shouldReturnSameThetaWhenNumItersIsZero();
+%! shouldReturnCorrectValuesForThetaVector();
+endfunction
