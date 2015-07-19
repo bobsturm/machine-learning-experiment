65273692ac53873dc43beec8873ba47a057cd72e
bobsturm <bobsturm@gmail.com>
2015-07-19 17:30:20 -0500

    no message

--- octave/executeModel.m
+++ octave/executeModel.m
@@ -1,9 +1,10 @@
-function [J_train, J_val, theta] = executeModel(X_train, y_train, X_val, y_val, learn_func, cost_func)
+function [J_train, J_val, theta] = executeModel(X_train, y_train, X_val, y_val, theta, learn_func, cost_func)
 
 m_train = size(X_train, 1);
 n_train = size(X_train, 2);
 m_val = size(X_val, 1);
 n_val = size(X_val, 2);
+theta_length = length(theta);
 
 if (size(y_train, 2) != 1)
 	error("expected row vector for y_train");
@@ -25,14 +26,27 @@ if (m_val != length(y_val))
 	error("number of y_val entries != validation entries");
 endif
 
+if (n_val != (theta_length - 1) || n_train != (theta_length - 1))
+  error("theta row vector does not match to the training set feature #s or the validation set feature #s");
+endif
+
 X_train_norm = [ones(m_train, 1) X_train];
-
-theta = learn_func(X_train_norm, y_train);
-
-J_train = cost_func(X_train_norm, y_train, theta);
 
 X_val_norm = [ones(m_val, 1) X_val];
 
 J_val = cost_func(X_val_norm, y_val, theta);
 
+fprintf("Pre-training cost is %d against validation set with theta = \n", J_val);
+fprintf("%d\n", theta);
+
+theta = learn_func(X_train_norm, y_train, theta);
+
+J_train = cost_func(X_train_norm, y_train, theta);
+
+fprintf("Post-training cost is %d against training set with theta = \n", J_train);
+fprintf("%d\n", theta);
+
+J_val = cost_func(X_val_norm, y_val, theta);
+
+fprintf("Post-training cost is %d against validation set (theta same as above)\n", J_val);
 endfunction
--- octave/testExecuteModel.m
+++ octave/testExecuteModel.m
@@ -2,30 +2,34 @@ function testExecuteModel
 	assert(false, 'This function should only be called in test mode!');
 
 %!function testInvalidInputs
-%! fail('executeModel([2 2; 3 3], [3 2;3 2], [2 2], [3], @gradientDescent, @computeCostGrad)', 
+%! fail('executeModel([2 2; 3 3], [3 2;3 2], [2 2], [3], [0; 0], @gradientDescent, @computeCostGrad)', 
 %!	"expected row vector for y_train");
 %!
-%! fail('executeModel([2 2; 3 3], [3;2], [2 2], [3 2], @gradientDescent, @computeCostGrad)', 
+%! fail('executeModel([2 2; 3 3], [3;2], [2 2], [3 2], [0; 0], @gradientDescent, @computeCostGrad)', 
 %!	"expected row vector for y_val");
 %!
-%! fail('executeModel([2;3], [3;3], [2 2], [3], @gradientDescent, @computeCostGrad)', 
+%! fail('executeModel([2;3], [3;3], [2 2], [3], [0; 0], @gradientDescent, @computeCostGrad)', 
 %!	"number of features in training set != number of features in validation set");
 %!
-%! fail('executeModel([2 2; 3 3], [3], [2 2], [3], @gradientDescent, @computeCostGrad)', 
+%! fail('executeModel([2 2; 3 3], [3], [2 2], [3], [0; 0], @gradientDescent, @computeCostGrad)', 
 %!	"number of y_train entries != training entries");
 %!
-%! fail('executeModel([2 2; 3 3], [3;2], [2 2], [3;1], @gradientDescent, @computeCostGrad)', 
+%! fail('executeModel([2 2; 3 3], [3;2], [2 2], [3;1], [0; 0], @gradientDescent, @computeCostGrad)', 
 %!	"number of y_val entries != validation entries");
+%!
+%! fail('executeModel([2 2; 3 3], [3;2], [2 2], [3], [0; 0], @gradientDescent, @computeCostGrad)', 
+%!	"theta row vector does not match to the training set feature #s or the validation set feature #s");
 %!endfunction
 
 %!function testBasicCall
 %! X = [1 1 1; 2 2 2; 3 3 3];
 %! y = [10; 8; 6];
-%! [J_train, J_val, theta_r] = executeModel(X, y, X, y,
-%!		@(X, y)gradientDescent(X, y, [0;0;0;0], 1, 3), @computeCostGrad);
-%! theta = [1160; 2670.667; 2670.667; 2670.667];
-%! assert(theta_r, theta, 0.001);
 %! X_with_bias = [ones(size(X, 1), 1) X];
+%! theta = [0; 0; 0; 0];
+%! [theta_grad_desc, J_hist] = gradientDescent(X_with_bias, y, theta, .1, 50);
+%! [J_train, J_val, theta_r] = executeModel(X, y, X, y, theta,
+%!		@(X, y, theta)gradientDescent(X, y, theta, .1, 50), @computeCostGrad);
+%! assert(theta_r, theta_grad_desc);
 %! assert(J_train, computeCostGrad(X_with_bias, y, theta_r));
 %! assert(J_val, computeCostGrad(X_with_bias, y, theta_r));
 %!endfunction
